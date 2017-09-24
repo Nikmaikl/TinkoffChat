090c5413524fe6b3669170d521495e918fd0689a
Michael Nikolaev <minikolev@MacBook-Pro-Michael.local>
2017-09-24 12:44:45 +0300

    Added first files

---
+++ TinkoffChat/ProfileViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Michael Nikolaev on 20.09.17.
+//  Copyright © 2017 Michael Nikolaev. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+}
+
